5b38caf316f00901bc8e361dcfd31f87d118b8de
shamsulhaquesami01 <2305055@ugrad.cse.buet.ac.bd>
2025-12-24 03:25:17 +0600

    yeyyeee

--- SQL216/ONLINE-1(DATE,STRING,AGG)/21.sql
+++ SQL216/ONLINE-1(DATE,STRING,AGG)/21.sql
@@ -28,3 +28,29 @@ select sum(8100-salary) from EMPLOYEES
 -- Write a single query to find the total number of employees who are charged with
 -- commissions and who are not. The Indicator here is boolean that shows 0 for not being
 -- charged and 1 for being charged. Your output should exactly match the following:
+
+select 
+(case 
+  when COMMISSION_PCT is  null then 0||'(No)'
+  else 1||'(YES)'
+  end
+) as "IS_COMMISSION_CHARGED?", count(*) as total from EMPLOYEES
+   GROUP by (case 
+  when COMMISSION_PCT is  null then 0||'(No)'
+  else 1||'(YES)'
+  end
+);
+
+-- For each job, print the range of salary (difference between minimum and maximum salary) in
+-- that job for the jobs that have more than one employee. Order the result by descending
+-- range of salary.
+-- Example output:
+
+SELECT job_id, max(salary)-min(salary) as range from EMPLOYEES
+        group by JOB_ID
+        having count(*) > 1
+        order by range desc;
+
+Show all the employees' employee id, last name and annual salary who are from department
+60 or 80 or 100 and have an annual salary greater than 100000 in descending order of their
+annual salary and then ascending order of their first name.
