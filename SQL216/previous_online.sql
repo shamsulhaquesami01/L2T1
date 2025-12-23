f9347566f4f5f20094de1dfcad7b1e587ef80aeb
sami <shamsulhaquesami@gmail.com>
2025-12-23 13:26:14 +0600

    pratice

--- SQL216/previous_online.sql
+++ SQL216/previous_online.sql
@@ -5,6 +5,8 @@ name starts with ‘D’ and the fourth character of the last name is ‘n’ an
 departments with ID between 20 and 70. (Assume zero commission percentage
 for the employees with null commission percentage)
 */
-
+SELECT employee_id, first_name ||' '|| last_name full_name, department_id, salary+(salary*nvl(commission_PCT,0)) "SALARY"
+        FROM employees
+        WHERE FIRST_NAME LIKE 'D%' AND LAST_NAME LIKE '___n%' AND (DEPARTMENT_ID BETWEEN 20 and 70);
 
 
