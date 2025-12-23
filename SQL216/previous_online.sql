86d2547c0501c9f6408c4fc32dc4d6d918264183
sami <shamsulhaquesami@gmail.com>
2025-12-23 13:34:54 +0600

    hello

--- SQL216/previous_online.sql
+++ SQL216/previous_online.sql
@@ -9,4 +9,37 @@ SELECT employee_id, first_name ||' '|| last_name full_name, department_id, salar
         FROM employees
         WHERE FIRST_NAME LIKE 'D%' AND LAST_NAME LIKE '___n%' AND (DEPARTMENT_ID BETWEEN 20 and 70);
 
+/*
+2. Display the job IDs of jobs that were held by at least two employees for a duration
+of at most 1500 days.
+3. Show the department_id, minimum, maximum, and average salary of all
+departments except depeartment_id 50. Sort the list in descending order of the
+difference between maximum salary and minimum salary of each department
+and then sort by average salary in ascending order. (The average salary value is
+rounded to four decimal digits)
+4. For each manager who manages less than 10 employees, show the manager id,
+number of employees and the average salary of the employees managed by
+him/her. Show the outputs with an average salary less than 4000. Print the output
+in the ascending order of number of employees managed and if there is a tie then
+print those in descending order of average salary. Make sure any sort of null value
+is not printed.
+5. Find the last name of those employees whose last name contains odd number of
+consonants.
+*/
 
+--2
+
+
+select job_id 
+  from EMPLOYEES 
+  where (sysdate-HIRE_DATE) <=1500
+  group by JOB_id
+  HAVING count(*) >=2;
+
+SELECT JOB_ID
+FROM JOB_HISTORY
+WHERE (END_DATE - START_DATE) <= 1500
+GROUP BY JOB_ID
+HAVING COUNT(*) >= 2;
+
+--3
