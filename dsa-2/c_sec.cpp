76b56ba33fbccca70433e449f559d7c577cd13f5
shamsulhaquesami01 <2305055@ugrad.cse.buet.ac.bd>
2026-01-06 13:28:14 +0600

    online

--- dsa-2/c_sec.cpp
+++ dsa-2/c_sec.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <math.h>
 #include <algorithm>
 using namespace std;
-using ll = long long;
-const ll INF = 1e18;
 
 struct Edge {
     int to;
-    ll w;
+    int w;
 };
 
 struct State {
-    ll dist;
+    int dist;
     int node, used;
     bool operator>(const State& other) const {
         return dist > other.dist;
     }
 };
-int solve(vector<vector<Edge>>& adj, int n){
-vector<vector<ll>> dp(n+1, vector<ll>(2, INF));
-    dp[1][0] = 0; // start at Dhaka, coupon unused
+pair<int,int> solve(int start, vector<vector<Edge>>& adj, int n,int dest){
+vector<vector<int>> dp(n+1, vector<int>(2, INT_MAX));
+    dp[start][0] = 0; // start at Dhaka, coupon unused
 
     priority_queue<State, vector<State>, greater<State>> pq;
-    pq.push({0,1,0});
+    pq.push({0,start,0});
 
     while(!pq.empty()) {
         auto [d,u,used] = pq.top(); pq.pop();
         if(d != dp[u][used]) continue;
 
         for(auto &e: adj[u]) {
-            int v = e.to; ll w = e.w;
+            int v = e.to; int w = e.w;
             // case 1: don't use coupon
             if(dp[v][used] > d + w) {
                 dp[v][used] = d + w;
@@ -38,26 +36,30 @@ vector<vector<ll>> dp(n+1, vector<ll>(2, INF));
             }
             // case 2: use coupon if not used yet
             if(used==0 && dp[v][1] > d + w/2) {
-                dp[v][1] = d + w/2;
+                dp[v][1] = d + floor(w/2);
                 pq.push({dp[v][1], v, 1});
             }
         }
     }
 
-    return min(dp[n][0], dp[n][1]) ;
+    return {dp[dest][0], dp[dest][1]} ;
 }
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     int n, m;
     cin >> n >> m;
     vector<vector<Edge>> adj(n+1);
-
+   vector<vector<Edge>> rev_adj(n+1);
     for(int i=0;i<m;i++) {
-        int a,b; ll c;
+        int a,b; int c;
         cin >> a >> b >> c;
         adj[a].push_back({b,c});
+        rev_adj[b].push_back({a,c});
     }
-    solve(adj,n);
+    int x,y,w; cin>>x>>y>>w;
+    auto [first_un, first_used]=solve(1,adj,n,x);
+    auto [second_unused, second_used]=solve(n,rev_adj,n,y);
+    //minimum of these 3 
+    cout<<first_un+w+second_used<<endl;
+    cout<<first_used + w + second_unused<<endl;
+    cout<<first_un+ floor(w/2) +second_unused<<endl;
 }
