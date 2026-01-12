beb61b7ced4b793f604e6e89584d96500fc3b32d
shamsulhaquesami01 <2305055@ugrad.cse.buet.ac.bd>
2026-01-12 23:08:20 +0600

    format

--- dsa-2/APSP/OFFLINE/2305055_problem02.cpp
+++ dsa-2/APSP/OFFLINE/2305055_problem02.cpp
@@ -1,56 +1,65 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include<map>
+#include <map>
 #include <algorithm>
 #include <climits>
 using namespace std;
 using ll = long long;
-void solve(){
-    int n; cin>>n;
-    map<string,int> currency;
-    for(int i=0; i<n; i++){
-        string s; cin>>s;
-        currency[s]=i;
+void solve()
+{
+    int n;
+    cin >> n;
+    map<string, int> currency;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        currency[s] = i;
     }
-    vector<vector <double> >dist(n, vector <double> (n, 0.0));
-       for (int i = 0; i < n; i++)
+    vector<vector<double>> dist(n, vector<double>(n, 0.0));
+    for (int i = 0; i < n; i++)
         dist[i][i] = 1.0;
-    int m; cin>>m;
-      for (int i = 0; i < m; i++){
-        string from,to;
+    int m;
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        string from, to;
         double rate;
-        cin>>from>>rate>>to;
-        int a = currency[from]; int b = currency[to];
-        dist[a][b] =rate;
-      }
+        cin >> from >> rate >> to;
+        int a = currency[from];
+        int b = currency[to];
+        dist[a][b] = rate;
+    }
 
-      for (int k = 0; k <n; k++)
+    for (int k = 0; k < n; k++)
     {
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-     
-                    dist[i][j] = max(dist[i][j], dist[i][k] * dist[k][j]);
-                
+
+                dist[i][j] = max(dist[i][j], dist[i][k] * dist[k][j]);
             }
         }
     }
-  
-    for(int i=0; i<n; i++){
-        if(dist[i][i]>1.0){
-            cout<<"Yes"<<endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (dist[i][i] > 1.0)
+        {
+            cout << "Yes" << endl;
             return;
         }
     }
-    cout<<"No"<<endl;
+    cout << "No" << endl;
 }
 
-int main(){
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     solve();
 }
 
-//g++ -std=c++17 dsa-2/APSP/OFFLINE/problem02.cpp -o solution && ./solution < dsa-2/APSP/OFFLINE/input.txt
+// g++ -std=c++17 dsa-2/APSP/OFFLINE/problem02.cpp -o solution && ./solution < dsa-2/APSP/OFFLINE/input.txt
