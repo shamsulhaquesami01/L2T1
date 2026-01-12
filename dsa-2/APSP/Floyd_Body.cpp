c352f3953087c0fadec139a5ee87d9ba6a674199
shamsulhaquesami01 <2305055@ugrad.cse.buet.ac.bd>
2026-01-12 22:28:33 +0600

    online

---
+++ dsa-2/APSP/Floyd_Body.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <climits>
+using namespace std;
+using ll = long long;
+const ll INF = 1e18;
+
+void solve(vector<vector<ll> >& dist,int n)
+{
+
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (dist[i][k] != INF && dist[k][j] != INF)
+                {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n, m;
+    cin >> n >> m ;
+    vector<vector<ll> > dist(n + 1, vector<ll>(n + 1, INF));
+    for (int i = 1; i <= n; i++)
+        dist[i][i] = 0;
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        ll w;
+        cin >> a >> b >> w;
+        dist[a][b] = min(dist[a][b], w);
+    }
+
+    int q; cin>>q;
+    for (int i = 0; i < q; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (dist[a][b] != INF)
+            cout << dist[a][b]<<endl;
+        else
+            cout << -1<<endl;
+    }
+    
+
+}
+
+//g++ -std=c++17 dsa-2/APSP/OFFLINE/problem01.cpp -o solution && ./solution < dsa-2/APSP/OFFLINE/input.txt
