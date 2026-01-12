53f80225c63115c664ac734dcee88ebef00f2e2f
shamsulhaquesami01 <2305055@ugrad.cse.buet.ac.bd>
2026-01-12 23:02:40 +0600

    okay

--- dsa-2/APSP/Transitive_Clousre.cpp
+++ dsa-2/APSP/Transitive_Clousre.cpp
@@ -3,34 +3,39 @@
 #include <queue>
 #include <algorithm>
 #include <climits>
-#include<bitset>
+#include <bitset>
 using namespace std;
 using ll = long long;
 const ll INF = 1e18;
 const int MAXN = 505;
 
-void solve(vector<vector<bool> >& reach,int n)
+void solve(vector<vector<bool>> &reach, int n)
 {
-
+    // Slower, standard version
     for (int k = 1; k <= n; k++)
     {
         for (int i = 1; i <= n; i++)
         {
-            for (int j = 1; j <= n; j++)
+            if (reach[i][k])
             {
-                if (reach[i][k] != INF && reach[k][j] != INF)
+                for (int j = 1; j <= n; j++)
                 {
-                    reach[i][j] = reach[i][j]||( reach[i][k] && reach[k][j]);
+                    if (reach[k][j])
+                        reach[i][j] = true;
                 }
             }
         }
     }
 }
-void optimized_solve(vector<bitset<MAXN>>& reach2,int n){
-   for (int k = 1; k <= n; k++) {
-        for (int i = 1; i <= n; i++) {
+void optimized_solve(vector<bitset<MAXN>> &reach2, int n)
+{
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
             // If i can reach k, then i can also reach everything k can reach.
-            if (reach2[i][k]) {
+            if (reach2[i][k])
+            {
                 reach2[i] |= reach2[k]; // <--- This now WORKS and is FAST
             }
         }
@@ -41,15 +46,15 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     ll n, m;
-    cin >> n >> m ;
-    vector<vector<bool> > reach(n + 1, vector<bool>(n + 1, false));
+    cin >> n >> m;
+    vector<vector<bool>> reach(n + 1, vector<bool>(n + 1, false));
 
     vector<bitset<MAXN>> reach2(n + 1);
 
-
-    for (int i = 1; i <= n; i++){
+    for (int i = 1; i <= n; i++)
+    {
         reach[i][i] = true;
-        reach2[i][i]=1;
+        reach2[i][i] = 1;
     }
     for (int i = 0; i < m; i++)
     {
@@ -57,19 +62,19 @@ int main()
         ll w;
         cin >> a >> b >> w;
         reach[a][b] = true;
-        reach2[a][b] =1;
+        reach2[a][b] = 1;
     }
 
-    solve(reach,n);
+    solve(reach, n);
     cout << "Transitive Closure Matrix:" << endl;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
             cout << (reach[i][j] ? "1 " : "0 ");
         }
         cout << endl;
     }
-    
-
 }
 
-//g++ -std=c++17 dsa-2/APSP/OFFLINE/problem01.cpp -o solution && ./solution < dsa-2/APSP/OFFLINE/input.txt
+// g++ -std=c++17 dsa-2/APSP/OFFLINE/problem01.cpp -o solution && ./solution < dsa-2/APSP/OFFLINE/input.txt
