531f1a9fc777cc217e8130b93ae642e5f57d51aa
shamsulhaquesami01 <2305055@ugrad.cse.buet.ac.bd>
2026-01-14 11:55:04 +0600

    naznin mam ashtese

--- dsa-2/APSP/APSP VARIATION/Mandatory_edge.cpp
+++ dsa-2/APSP/APSP VARIATION/Mandatory_edge.cpp
@@ -9,7 +9,6 @@ const ll INF = 1e18;
 
 void solve(vector<vector<ll> >& dist,int n)
 {
-
     for (int k = 1; k <= n; k++)
     {
         for (int i = 1; i <= n; i++)
@@ -24,34 +23,44 @@ void solve(vector<vector<ll> >& dist,int n)
         }
     }
 }
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    ll n, m;
+    
+    int n, m; // Changed n to int (safe for array sizing loop)
     cin >> n >> m ;
+    
     vector<vector<ll> > dist(n + 1, vector<ll>(n + 1, INF));
     for (int i = 1; i <= n; i++)
         dist[i][i] = 0;
+        
     for (int i = 0; i < m; i++)
     {
         int a, b;
         ll w;
         cin >> a >> b >> w;
         dist[a][b] = min(dist[a][b], w);
-    }
-    int y,z; cin>>y>>z;
-    int mandatory_weight = dist[y][z];
-    int q; cin>>q;
-    for(int i=0; i<q; i++){
-        int c,d; cin>>c>>d;
-        if(mandatory_weight != INF && dist[c][y] != INF && dist[z][d] != INF)
-            cout<<dist[c][y]+mandatory_weight+dist[z][d]<<endl;
-        
-        else cout<<"-1"<<endl;
+        // dist[b][a] = min(dist[b][a], w); 
     }
     
+    int y, z; 
+    cin >> y >> z;
+    ll mandatory_weight = dist[y][z]; // FIX: Changed int to ll
+    solve(dist, n);
+    
+    int q; 
+    cin >> q;
+    for(int i = 0; i < q; i++){
+        int c, d; 
+        cin >> c >> d;
+        
 
+        if(mandatory_weight != INF && dist[c][y] != INF && dist[z][d] != INF)
+            cout << dist[c][y] + mandatory_weight + dist[z][d] << endl;
+        else 
+            cout << "-1" << endl;
+    }
+    return 0;
 }
-
-//g++ -std=c++17 dsa-2/APSP/OFFLINE/problem01.cpp -o solution && ./solution < dsa-2/APSP/OFFLINE/input.txt
