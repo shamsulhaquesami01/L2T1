a0381205749f8320ec7db862c40bce3695d39823
shamsulhaquesami01 <2305055@ugrad.cse.buet.ac.bd>
2026-01-26 20:19:27 +0600

    1153

---
+++ DSA-II/FLOW/LIGHTOJ/1153-Internet_Bandwith.cpp
@@ -0,0 +1,88 @@
+// Direct Max Flow The Subtle Logic: This is the most basic definition of Max Flow. 
+// The only "trick" is that there can be multiple cables between two node
+
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+// --- TEMPLATE ---
+struct Edge { int to, capacity, flow, rev; };
+class MaxFlow {
+    int N;
+    vector<vector<Edge>> graph;
+public:
+    MaxFlow(int n) : N(n), graph(n) {}
+    int addEdge(int u, int v, int cap) {
+        Edge a = {v, cap, 0, (int)graph[v].size()};
+        Edge b = {u, 0, 0, (int)graph[u].size()};
+        graph[u].push_back(a);
+        graph[v].push_back(b);
+        return (int)graph[u].size() - 1;
+    }
+    int bfs(int s, int t, vector<pair<int,int>>& parent) {
+        fill(parent.begin(), parent.end(), make_pair(-1, -1));
+        queue<int> q;
+        q.push(s); parent[s] = {s, -1};
+        while (!q.empty()) {
+            int u = q.front(); q.pop();
+            for (int i = 0; i < graph[u].size(); i++) {
+                Edge &e = graph[u][i];
+                if (parent[e.to].first == -1 && e.capacity > e.flow) {
+                    parent[e.to] = {u, i};
+                    if (e.to == t) return 1;
+                    q.push(e.to);
+                }
+            }
+        }
+        return 0;
+    }
+    int edmondsKarp(int s, int t) {
+        int maxFlow = 0;
+        vector<pair<int,int>> parent(N);
+        while (bfs(s, t, parent)) {
+            int pathFlow = INT_MAX;
+            for (int v = t; v != s; ) {
+                auto [u, idx] = parent[v];
+                pathFlow = min(pathFlow, graph[u][idx].capacity - graph[u][idx].flow);
+                v = u;
+            }
+            for (int v = t; v != s; ) {
+                auto [u, idx] = parent[v];
+                graph[u][idx].flow += pathFlow;
+                int rev_idx = graph[u][idx].rev;
+                graph[v][rev_idx].flow -= pathFlow;
+                v = u;
+            }
+            maxFlow += pathFlow;
+        }
+        return maxFlow;
+    }
+};
+
+
+void solve(){
+    int t ; cin>>t;
+    for(int i=1; i<=t; i++){
+        int n;
+        cin>>n;
+        MaxFlow mf(n+1);
+        int s,T,m;
+        cin>>s>>T>>m;
+        for(int i=0; i<m; i++){
+            int a,b,c;
+            cin>>a>>b>>c;
+            mf.addEdge(a,b,c);
+            mf.addEdge(b,a,c);
+        }
+        cout<<"Case "<<i<<":"<<mf.edmondsKarp(s,T)<<endl;
+    }
+}
+int main(){
+    solve();
+}
